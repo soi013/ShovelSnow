1632c69de2ebb3382ff15da58864b3dd1a93cd1c
soi013 <soi2flatline@gmail.com>
2022-05-08 19:49:26 +0900

    名前空間を追記

--- src/Assets/__Projects/Tests/EditMode/AppModel_Tests.cs
+++ src/Assets/__Projects/Tests/EditMode/AppModel_Tests.cs
@@ -3,44 +3,47 @@ using JPLab2.Model;
 using NUnit.Framework;
 using UnityEngine;
 
-public class AppModel_Tests
+namespace JPLab2.Tests
 {
-    [Test]
-    public void AppState_NormalOld_StartToEnd()
+    public class AppModel_Tests
     {
-        var playerM = new PlayerModel();
-        var appM = new AppModel(playerM);
+        [Test]
+        public void AppState_NormalOld_StartToEnd()
+        {
+            var playerM = new PlayerModel();
+            var appM = new AppModel(playerM);
 
-        //デフォルト状態は初期化中
-        Assert.AreEqual(appM.State.Value, AppState.Initializing);
+            //デフォルト状態は初期化中
+            Assert.AreEqual(appM.State.Value, AppState.Initializing);
 
-        //初期化処理するとプレイ中
-        appM.Initialize();
-        Assert.AreEqual(appM.State.Value, AppState.Playing);
+            //初期化処理するとプレイ中
+            appM.Initialize();
+            Assert.AreEqual(appM.State.Value, AppState.Playing);
 
-        //台から落ちたらゲームオーバー
-        playerM.Position.Value = new Vector3(0, -100, 0);
-        Assert.AreEqual(appM.State.Value, AppState.GameOver);
-    }
+            //台から落ちたらゲームオーバー
+            playerM.Position.Value = new Vector3(0, -100, 0);
+            Assert.AreEqual(appM.State.Value, AppState.GameOver);
+        }
 
-    [Test]
-    public void AppState_NormalFluent_StartToEnd()
-    {
-        var playerM = new PlayerModel();
-        var appM = new AppModel(playerM);
+        [Test]
+        public void AppState_NormalFluent_StartToEnd()
+        {
+            var playerM = new PlayerModel();
+            var appM = new AppModel(playerM);
 
-        //デフォルト状態は初期化中
-        appM.State.Value
-            .Should().Be(AppState.Initializing);
+            //デフォルト状態は初期化中
+            appM.State.Value
+                .Should().Be(AppState.Initializing);
 
-        //初期化処理するとプレイ中
-        appM.Initialize();
-        appM.State.Value
-            .Should().Be(AppState.Playing);
+            //初期化処理するとプレイ中
+            appM.Initialize();
+            appM.State.Value
+                .Should().Be(AppState.Playing);
 
-        //台から落ちたらゲームオーバー
-        playerM.Position.Value = new Vector3(0, -100, 0);
-        appM.State.Value
-            .Should().Be(AppState.GameOver);
+            //台から落ちたらゲームオーバー
+            playerM.Position.Value = new Vector3(0, -100, 0);
+            appM.State.Value
+                .Should().Be(AppState.GameOver);
+        }
     }
 }
--- src/Assets/__Projects/Tests/EditMode/PlayerModel_Tests.cs
+++ src/Assets/__Projects/Tests/EditMode/PlayerModel_Tests.cs
@@ -1,22 +1,23 @@
-﻿
-using FluentAssertions;
+﻿using FluentAssertions;
 using JPLab2.Model;
 using NUnit.Framework;
 using UnityEngine;
-
-public class PlayerModel_Tests
+namespace JPLab2.Tests
 {
-    [Test]
-    public void IsDead_UnderPostion_Dead()
+    public class PlayerModel_Tests
     {
-        var playerM = new PlayerModel();
+        [Test]
+        public void IsDead_UnderPostion_Dead()
+        {
+            var playerM = new PlayerModel();
 
-        playerM.IsDead.Value
-           .Should().BeFalse();
+            playerM.IsDead.Value
+               .Should().BeFalse();
 
-        //台から落ちたらゲームオーバー
-        playerM.Position.Value = new Vector3(0, -100, 0);
-        playerM.IsDead.Value
-            .Should().BeTrue();
+            //台から落ちたらゲームオーバー
+            playerM.Position.Value = new Vector3(0, -100, 0);
+            playerM.IsDead.Value
+                .Should().BeTrue();
+        }
     }
 }
